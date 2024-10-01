fd8807c64589035f6b1cc5b21ec0638cd30ed2b7
harishselvan <harishselvank@gmail.com>
2024-10-01 16:17:44 +0530

    Add files via upload

---
+++ conversionautomatic.java
@@ -0,0 +1,9 @@
+class conversionautomatic{
+ public static void main (String[] args) {
+    int i=100;
+    long l=1;
+    float f=1;
+    System.out.println("long value"*i);
+    System.out.println("float value"=f);
+   }
+}
